cf277b0f2e8d720e7674893c46ebc0b2241714d9
Your New Name <aullah@tulip-tech.com>
2025-12-16 22:34:06 +0600

    fix: update application.yml for environment variable support and modify pom.xml for Docker integration

--- src/main/java/com/Tulip_Tech/ProductService/ProductServiceApplication.java
+++ src/main/java/com/Tulip_Tech/ProductService/ProductServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductServiceApplication {
 
-     static void main(String[] args) {
+     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
--- src/test/java/com/Tulip_Tech/ProductService/ProductEntityServiceApplicationTests.java
+++ src/test/java/com/Tulip_Tech/ProductService/ProductEntityServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.Tulip_Tech.ProductService;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductEntityServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
